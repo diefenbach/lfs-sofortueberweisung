adf2e7eca60208312089e7058f7d7f77680f0bc2
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-01-15 13:58:30 +0100

    Take order from the instance.

--- lfs_sofortueberweisung/models.py
+++ lfs_sofortueberweisung/models.py
@@ -3,24 +3,24 @@ from django.conf import settings
 
 # lfs imports
 from lfs.cart.utils import get_cart
-from lfs.plugins import PaymentMethod
+from lfs.plugins import PaymentMethodProcessor
 from lfs.plugins import PM_ORDER_IMMEDIATELY
 
 
-class SofortUeberweisungPaymentMethod(PaymentMethod):
+class SofortUeberweisungPaymentMethodProcessor(PaymentMethodProcessor):
     """
     Provides payment processment with sofortueberweisung.de.
     """
-    def process(self, request, cart=None, order=None):
+    def process(self):
         return {
             "accepted": True,
             "next_url": "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=Project&reason_2=Test&amount=%s&currency=EUR" % \
-                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, order.price)
+                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, self.order.price)
         }
 
-    def get_pay_link(self, order):
+    def get_pay_link(self):
         return "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=Project&reason_2=Test&amount=%s&currency=EUR" % \
-                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, order.price)
+                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, self.order.price)
 
     def get_create_order_time(self):
         return PM_ORDER_IMMEDIATELY
