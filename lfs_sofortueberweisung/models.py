9b09ba30ff7e39c87e8f6d55a480831d7abb09e2
Kai Diefenbach <kai.diefenbach@iqpp.de>
2012-01-26 08:26:22 +0100

    Use order numbe for reason_1

--- lfs_sofortueberweisung/models.py
+++ lfs_sofortueberweisung/models.py
@@ -14,13 +14,13 @@ class SofortUeberweisungPaymentMethodProcessor(PaymentMethodProcessor):
     def process(self):
         return {
             "accepted": True,
-            "next_url": "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=Project&reason_2=Test&amount=%s&currency=EUR" % \
-                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, self.order.price)
+            "next_url": "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=%s&amount=%s&currency=EUR" % \
+                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, self.order.number, self.order.price)
         }
 
     def get_pay_link(self):
-        return "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=Project&reason_2=Test&amount=%s&currency=EUR" % \
-                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, self.order.price)
+        return "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=%s&amount=%s&currency=EUR" % \
+                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, self.order.number, self.order.price)
 
     def get_create_order_time(self):
         return PM_ORDER_IMMEDIATELY
