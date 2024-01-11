74752c9496a6da7533c7246c9271bbb6b7151003
Kai Diefenbach <kai.diefenbach@iqpp.de>
2024-01-11 14:38:58 +0100

    blacken

--- lfs_sofortueberweisung/models.py
+++ lfs_sofortueberweisung/models.py
@@ -11,16 +11,29 @@ class SofortUeberweisungPaymentMethodProcessor(PaymentMethodProcessor):
     """
     Provides payment processment with sofortueberweisung.de.
     """
+
     def process(self):
         return {
             "accepted": True,
-            "next_url": "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=Bestellnummer %s&amount=%s&currency=EUR" % \
-                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, self.order.number, self.order.price)
+            "next_url": "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=Bestellnummer %s&amount=%s&currency=EUR"
+            % (
+                settings.SOFORTUEBERWEISUNG_USERID,
+                settings.SOFORTUEBERWEISUNG_PROJECT_ID,
+                self.order.number,
+                self.order.price,
+            ),
         }
 
     def get_pay_link(self):
-        return "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=Bestellnummer %s&amount=%s&currency=EUR" % \
-                (settings.SOFORTUEBERWEISUNG_USERID, settings.SOFORTUEBERWEISUNG_PROJECT_ID, self.order.number, self.order.price)
+        return (
+            "https://www.sofortueberweisung.de/payment/start?user_id=%s&project_id=%s&reason_1=Bestellnummer %s&amount=%s&currency=EUR"
+            % (
+                settings.SOFORTUEBERWEISUNG_USERID,
+                settings.SOFORTUEBERWEISUNG_PROJECT_ID,
+                self.order.number,
+                self.order.price,
+            )
+        )
 
     def get_create_order_time(self):
         return PM_ORDER_IMMEDIATELY
--- setup.py
+++ setup.py
@@ -1,31 +1,32 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.0b1'
+version = "1.0b1"
 
 here = os.path.abspath(os.path.dirname(__file__))
-README = open(os.path.join(here, 'README.txt')).read()
+README = open(os.path.join(here, "README.txt")).read()
 
-setup(name='lfs_sofortueberweisung',
-      version=version,
-      description='Payment processor sofortueberweisung.de for LFS',
-      long_description=README,
-      classifiers=[
-          'Environment :: Web Environment',
-          'Framework :: Django',
-          'License :: OSI Approved :: BSD License',
-          'Operating System :: OS Independent',
-          'Programming Language :: Python',
-      ],
-      keywords='django e-commerce online-shop',
-      author='Kai Diefenbach',
-      author_email='kai.diefenbach@iqpp.de',
-      url='http://www.getlfs.com',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-        'setuptools',
-      ],
-      )
+setup(
+    name="lfs_sofortueberweisung",
+    version=version,
+    description="Payment processor sofortueberweisung.de for LFS",
+    long_description=README,
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+    ],
+    keywords="django e-commerce online-shop",
+    author="Kai Diefenbach",
+    author_email="kai.diefenbach@iqpp.de",
+    url="http://www.getlfs.com",
+    license="BSD",
+    packages=find_packages(exclude=["ez_setup"]),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "setuptools",
+    ],
+)
